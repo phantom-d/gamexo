e3c2340bd872c15918fa643608df0cf733041135
phantom <phantom@phantom-MSI>
2013-07-22 19:06:11 +0400

    START

---
+++ gamexo/player/Human.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gamexo.player;
+
+/**
+ *
+ * @author phantom
+ */
+public class Human {
+
+}
