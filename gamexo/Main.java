11e2a0e81e2d237acc720b5abc06fc8f156552e2
phantom <phantom@phantom-MSI>
2013-07-22 19:17:07 +0400

    * remove default comments from classes

--- gamexo/Main.java
+++ gamexo/Main.java
@@ -1,19 +1,8 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package gamexo;
 
-/**
- *
- * @author phantom
- */
 public class Main {
 
-	/**
-	 * @param args the command line arguments
-	 */
 	public static void main(String[] args) {
-		// TODO code application logic here
+		
 	}
 }
--- gamexo/player/Computer.java
+++ gamexo/player/Computer.java
@@ -1,13 +1,5 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package gamexo.player;
 
-/**
- *
- * @author phantom
- */
 public class Computer {
 
 }
